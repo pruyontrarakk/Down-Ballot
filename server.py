1e0a84e07251bcc398c831cf41523da45b3ff737
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-10-05 14:36:33 -0400

    start on user route

--- server.py
+++ server.py
@@ -1,15 +1,30 @@
 from flask import Flask, jsonify, request, render_template
+from openai_secrets import API_KEY
+from openai import OpenAI
+
+openai_client = OpenAI(api_key=API_KEY)
 
 app = Flask(__name__)
+
+
+
+user_info = {
+    "zipcode": 0,
+    "policy_interests": ["climate_change", "democracy", "healthcare", "education"]
+}
 
 @app.route('/')
 def home():
     return render_template('form.html')
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {"message": "Hello, World!"}
-    return jsonify(data)
+@app.route('/user', method=['POST', 'GET'])
+def user():
+    if request.method == 'POST':
+        user_info['zipcode'] = request.form['zipcode']
+        user_info['policy_interests'] = request.form['policy_interests']
+        return render_template('user.html', user_info=user_info)
+    else:
+        return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Your code here
     console.log("DOM is fully loaded and ready.");
+
+
+
+    $("user-info").on("submit", function (event) {
+        let formData = {
+            zipcode: $('#zipcode').val(),
+            state: $('#state').val(),
+            choices: []
+        };
+        $('input[name="choice"]:checked').each(function () {
+            formData.choices.push($(this).val());
+        });
+
+
+    })
 });
